f68e8b74cd0e5a2dfc9b5e7b07a4fc216e087a6b
BHARAT KUMAR <94525396+bharatk5003@users.noreply.github.com>
2022-01-28 15:35:45 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,34 +1,47 @@
-
-// const name=prompt("Enter your name to join");
 const socket= io('http://localhost:8000');
+// alert("Hello");
 
-// const name ="Bhanu";
-const form=document.getElementById('send-container')
-const messageInput=document.getElementById('messageInp')
-const messageContainer=document.querySelector('.container')
-// alert("hello");
+const form=document.getElementById('send-container');
+const messageInput=document.getElementById('messageInp');
+const messageContainer=document.querySelector('.container');
 
-const name=prompt("Please Enter your Name");
+var audio=new Audio('ting.mp3');
 
 const append=(message,position)=>{
-   const messageElement= document.createElement('div');
-   messageElement.innerText=message;
-   messageElement.classList.add('message');
-   messageElement.classList.add(position);
-   messageContainer.append(messageElement);
-
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left'){
+    audio.play();
+    }
 }
-/*
 
 
-socket.on('user-joined',data=>{
 
+
+const name=prompt("Enter Your Name :");
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined',name=>{
+   append(`${name} Joined the Chat`,'right');
+});
+
+socket.on('receive',data=>{
+    append(`${data.name}: ${data.message}`,'left');
+ });
+
+ socket.on('left',name=>{
+     append(`${name} left the chat`,'right');
+ })
+
+ 
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value='';
+     
 })
-*/
-
-// const name=prompt("Enter Your name");
-
-
-
-
-socket.emit('')
